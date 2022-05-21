a38b1301818cf317fecc2f5f1fff2b5b30bf35cf
phurwicz <pavelhurwicz@gmail.com>
2022-05-20 20:57:09 -0700

    Python API: bumped version to 0.1.1

--- python/setup.py
+++ python/setup.py
@@ -13,7 +13,7 @@ def get_description():
 
 setuptools.setup(
     name="credict",
-    version="0.1.0",
+    version="0.1.1",
     description="Python interface for Credict smart contracts on Ethereum.",
     long_description=get_description(),
     long_description_content_type="text/markdown",
